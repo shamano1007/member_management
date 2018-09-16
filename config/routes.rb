6a36eb0fc9083e6b4fe7a716dd06e3a82729decc
s-hamano <s.hamano1007@gmail.com>
2018-09-16 21:09:24 +0900

    ログイン画面作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#show'
+
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get :login, to: 'users/sessions#new', as: :new_user_session
+    post :login, to: 'users/sessions#create', as: :user_session
+    delete :logout, to: 'users/sessions#destroy', as: :destroy_user_session
+  end
+
 end
