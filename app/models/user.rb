10170647edaff990c410368e9c2cc454e778e535
s-hamano <s.hamano1007@gmail.com>
2018-09-17 17:57:12 +0900

    テスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # 参考
+  # https://qiita.com/cigalecigales/items/f4274088f20832252374#2-devise%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB%E6%A6%82%E8%A6%81
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :timeoutable
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
+RSpec.describe HomeController, type: :request do
   describe 'GET #show' do
-    it 'returns http success' do
-      get :show
-      expect(response).to have_http_status(:success)
+    context 'ログイン済み' do
+      login_user
+      before { get root_path }
+
+      it '成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context '未済み' do
+      before { get root_path }
+
+      it 'リダイレクトされること' do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(:new_user_session)
+      end
     end
   end
 end
