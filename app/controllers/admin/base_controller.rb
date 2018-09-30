9c3565db9bbbc5872dd7e54afe4c49e37fddfc3b
s-hamano <s.hamano1007@gmail.com>
2018-09-30 20:40:48 +0900

    管理権限設定追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,9 @@
 class Admin::BaseController < ApplicationController
+  before_action :authenticate_admin_user!
+
+  private
+
+  def authenticate_admin_user!
+    redirect_to root_path and return unless current_user.role.admin?
+  end
 end
