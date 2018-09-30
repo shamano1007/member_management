ce299b78c4d3d1bcbbd6c5d67dbd0f845b27fcc9
s-hamano <s.hamano1007@gmail.com>
2018-09-30 20:32:17 +0900

    ユーザー権限値変更

--- db/fixtures/00_users.rb
+++ db/fixtures/00_users.rb
@@ -1,6 +1,7 @@
 User.seed(:id) do |s|
   s.id    = 1
-  s.login_id = 'test'
-  s.password = 'password'
+  s.login_id = 'admin'
+  s.password = '12345678'
   s.name = 'テスト管理者'
+  s.role = :admin
 end
