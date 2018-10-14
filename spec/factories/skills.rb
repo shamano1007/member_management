13feb81b285031dc9ca4dd79af0a3f67f702267b
s-hamano <s.hamano1007@gmail.com>
2018-10-14 19:48:41 +0900

    modelテスト追加

--- spec/factories/skills.rb
+++ spec/factories/skills.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :skill do
+    name 'ruby'
+    remarks "オブジェクト指向スクリプト言語\nオブジェクト指向プログラミング"
   end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '入力チェック' do
+    let(:skill) { build(:skill) }
+
+    context '名前' do
+      check_for_required(FactoryBot.build(:skill), :name, 'ruby')
+    end
+  end
 end
